8bd03620d101b1cc79a9445f9d63c0f32b4dd711
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2018-02-28 16:40:29 +0000

    Imitial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+import "./sass/main.scss";
+
+const App = () => {
+  return ( 
+    <div>
+    <h1> Hello Parcel </h1> </div>
+  )
+};
+render( <App/> , document.getElementById("app"));
